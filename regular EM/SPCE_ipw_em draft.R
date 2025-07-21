764dabe6de6a3518899f05307d5cc1f85124f522
Ting Lin <Tarynlin0101@gmail.com>
2025-07-21 19:08:36 +0800

    revised

--- regular EM/SPCE_ipw_em draft.R
+++ regular EM/SPCE_ipw_em draft.R
@@ -1,4 +1,4 @@
-compute_IPW_from_EM <- function(res_em, X, Z) {
+compute_IPW_SPCE_from_EM <- function(res_em, X, Z, M, delta, t0) {
   # unpack converged treatment model parameters
   z.coef_final <- res_em$z.coef    
   U_final <- res_em$p             
@@ -6,7 +6,7 @@ compute_IPW_from_EM <- function(res_em, X, Z) {
   # fitted propensity scores P(Z=1|X,U)
   linpred <- cbind(1, X, U_final) %*% matrix(z.coef_final, ncol = 1)
   ps <- plogis(linpred)
-  
+  ps <- pmin(pmax(ps, 1e-6), 1 - 1e-6)
   # stabilized weights
   pZ <- mean(Z) # marginal treatment probability
   w <- (pZ * Z / ps) + ((1 - pZ) * (1 - Z) / (1 - ps))
@@ -15,5 +15,18 @@ compute_IPW_from_EM <- function(res_em, X, Z) {
   w <- pmin(w, 10)
   w <- pmax(w, 0.1)
   
-  return(w)
+  fit_w <- survfit(Surv(M, delta) ~ Z, weights = w)
+  surv_summary <- summary(fit_w, times = t0)
+  # Extract survival for Z=0 and Z=1
+  S_Z0 <- surv_summary$surv[surv_summary$strata == "Z=0"]
+  S_Z1 <- surv_summary$surv[surv_summary$strata == "Z=1"]
+  
+  SPCE <- S_Z1 - S_Z0
+  
+  return(list(
+    SPCE = SPCE,
+    S_Z1 = S_Z1,
+    S_Z0 = S_Z0,
+    weights = w
+  ))
 }
--- regular EM/em draft.R
+++ regular EM/em draft.R
@@ -1,25 +1,38 @@
 emU_surv <- function(t, d, z, x, gammat, gammaz, theta = 0.5, iter = 20){
   fn_outcome <- function(params, t, d, x, z, p, gammat) {
-    #unpack parameters
-    beta   <- params[1:ncol(x)]        # regression coefficients
-    alpha  <- params[ncol(x) + 1]      # treatment effect
-    sigma  <- params[ncol(x) + 2]      # scale parameter (> 0)
+    # Unpack params
+    beta      <- params[1:(ncol(x)+1)]
+    alpha     <- params[ncol(x)+2]
+    log_sigma <- params[ncol(x)+3]
+    sigma     <- exp(log_sigma)  # always >0
     
-    # linear predictor
-    xb <- as.vector(x %*% beta)
+    # Safe log(t)
+    log_t <- log(pmax(t, 1e-8))
     
-    # E[u_i] = p
-    lp_meanU <- xb + alpha * z + gammat * p
+    # Linear predictor
+    xb <- cbind(1, x) %*% beta
+    lp_meanU <- xb + alpha*z + gammat*p
     
-    # first term
-    term1 <- d * (-log(sigma) + ((1 / sigma - 1) * log(t)) - (1 / sigma) * lp_meanU)
+    # First term
+    term1 <- d * (-log(sigma) + ((1/sigma - 1)*log_t) - (1/sigma)*lp_meanU)
     
-    # second term
-    exp0 <- exp((log(t) - xb - alpha * z - gammat * 0) / sigma)
-    exp1 <- exp((log(t) - xb - alpha * z - gammat * 1) / sigma)
+    # Second term: prevent exp overflow
+    eta0 <- (log_t - xb - alpha*z - gammat*0)/sigma
+    eta1 <- (log_t - xb - alpha*z - gammat*1)/sigma
+    eta0 <- pmin(eta0, 50)
+    eta1 <- pmin(eta1, 50)
+    
+    exp0 <- exp(eta0)
+    exp1 <- exp(eta1)
     term2 <- p * exp1 + (1 - p) * exp0
+    term2 <- pmax(term2, 1e-12)  # avoid log(0)
     
-    return(-sum(term1 - term2))  # negative log-likelihood
+    nll <- -sum(term1 - term2)
+    
+    # If nll is non-finite, return huge penalty
+    if (!is.finite(nll)) return(1e12)
+    
+    return(nll)
   }
   
   
@@ -34,6 +47,7 @@ emU_surv <- function(t, d, z, x, gammat, gammaz, theta = 0.5, iter = 20){
     #negative tretment log-likelihood
     return(-sum(term1 + term2))
   }
+
   
   x = data.matrix(x)
   n = length(t) #number of observations
@@ -46,6 +60,7 @@ emU_surv <- function(t, d, z, x, gammat, gammaz, theta = 0.5, iter = 20){
   # extract initial values for next iteration
   t.coef1 = c(coef(U.fit1), gammat) #beta0...beta_nx, alpha,gammat
   sigma <- U.fit1$scale
+  init_params  <- c(coef(U.fit1), log(sigma)) 
   
   #fit treatment
   Z.fit1 <- glm(z ~ x, family = binomial(link = "logit"))
@@ -56,59 +71,55 @@ emU_surv <- function(t, d, z, x, gammat, gammaz, theta = 0.5, iter = 20){
   
   p = rep(0,n)
   
-  for(j in 1:iter){
+  for (j in 1:iter) {
     
-    # weibull aft linear prediction under U = 1 and U = 0
+    ## === E-step === ##
     mu_u1 <- cbind(1, x, z, 1) %*% matrix(t.coef1, ncol = 1)
     mu_u0 <- cbind(1, x, z, 0) %*% matrix(t.coef1, ncol = 1)
     alpha_weibull <- 1 / sigma
     
-    # Lambda (rate-like scale)
     lambda_u1 <- exp(-mu_u1)
     lambda_u0 <- exp(-mu_u0)
     
-    #weibull density and survival
-    lik_u1 <- ( alpha_weibull * lambda_u1^alpha_weibull * t^(alpha_weibull - 1) )^d *
-      exp( - (lambda_u1 * t)^alpha_weibull )
+    lik_u1 <- (alpha_weibull * lambda_u1^alpha_weibull * t^(alpha_weibull - 1))^d *
+      exp(-(lambda_u1 * t)^alpha_weibull)
+    lik_u0 <- (alpha_weibull * lambda_u0^alpha_weibull * t^(alpha_weibull - 1))^d *
+      exp(-(lambda_u0 * t)^alpha_weibull)
     
-    lik_u0 <- ( alpha_weibull * lambda_u0^alpha_weibull * t^(alpha_weibull - 1) )^d *
-      exp( - (lambda_u0 * t)^alpha_weibull )
+    logit_zu1 <- (1 - plogis(cbind(1, x, 1) %*% matrix(z.coef, ncol=1)))^(1 - z) *
+      plogis(cbind(1, x, 1) %*% matrix(z.coef, ncol=1))^z
+    logit_zu0 <- (1 - plogis(cbind(1, x, 0) %*% matrix(z.coef, ncol=1)))^(1 - z) *
+      plogis(cbind(1, x, 0) %*% matrix(z.coef, ncol=1))^z
     
-    # Logistic treatment model linear predictors
-    logit_zu1 <- (1 - plogis(cbind(1, x, 1) %*% matrix(z.coef, ncol = 1)))^(1 - z) *
-      plogis(cbind(1, x, 1) %*% matrix(z.coef, ncol = 1))^z
-    
-    logit_zu0 <- (1 - plogis(cbind(1, x, 0) %*% matrix(z.coef, ncol = 1)))^(1 - z) *
-      plogis(cbind(1, x, 0) %*% matrix(z.coef, ncol = 1))^z
-    
-    # Posterior numerator
     ptzu1 <- lik_u1 * logit_zu1 * theta
-    ptzu0 <- lik_u0 * logit_zu0 * (1-theta)
+    ptzu0 <- lik_u0 * logit_zu0 * (1 - theta)
     
-    # Posterior of U = 1
-    p <- ptzu1 / (ptzu1 + ptzu0) #vector of length n, different p_i for each individual
-    p[ptzu1 == 0 & ptzu0 == 0] <- 0  
+    p <- ptzu1 / (ptzu1 + ptzu0)
+    p[ptzu1 == 0 & ptzu0 == 0] <- 0
     
-    #fit time to event
-    init_params <- c(coef(U.fit1), sigma)
+    ## === M-step: time-to-event === ##
+    # use last params as init
     outcome_fit <- optim(par = init_params,
-                         fn = fn_outcome,
-                         t = t, d = d, x = x, z = z, p = p, gammat=gammat,
+                         fn  = fn_outcome,
+                         t   = t, d = d, x = x, z = z, p = p, gammat = gammat,
                          method = "BFGS",
                          control = list(maxit = 1000))
-    params <- outcome_fit$par
-    t.coef1 <- c(params[1:(ncol(x) + 2)], gammat)
-    sigma <- params[ncol(x) + 3]
-      
-    #fit treatment
+    
+    params  <- outcome_fit$par
+    t.coef1 <- c(params[1:(ncol(x)+2)], gammat)  # keep same design columns
+    sigma   <- exp(params[ncol(x)+3])            # back-transform log_sigma
+    init_params <- c(params[1:(ncol(x)+2)], log(sigma)) 
+    
+    ## === M-step: treatment model === ##
     z.fit <- optim(par = z.coef[1:(nx+1)],
-                   fn = fn_treat,
-                   x = x, z = z, p = p, gammaz = gammaz)
+                   fn  = fn_treat,
+                   x   = x, z = z, p = p, gammaz = gammaz)
     z.coef <- c(z.fit$par, gammaz)
     
-    t.coef1[is.na(t.coef1)] = 0
-    z.coef[is.na(z.coef)] = 0
-    }
+    t.coef1[is.na(t.coef1)] <- 0
+    z.coef[is.na(z.coef)]   <- 0
+  }
+  
   return(list(p = p, t.coef1 = t.coef1, z.coef = z.coef, sigma = sigma))
   #all coefficients
   }
